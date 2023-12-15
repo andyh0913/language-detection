78e7149309815e59c813f854afe3438b9042d95b
ZoneTwelve <zonetwelve159@gmail.com>
2023-12-15 09:23:01 +0800

    update: detection unit test

--- tests/detect.py
+++ tests/detect.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
+import unittest
 from langdetect import detect
 
-if __name__ == "__main__":
-  text = "Hello world, 我叫做 Wilson"
-  
-  result_zh = detect(text, lang='zh')
-  print('zh:', result_zh)
+class TestLanguageDetection(unittest.TestCase):
+    def test_normal_detection(self):
+        text = "Hello, world! 我喜歡 Coding。"
+        result = detect(text)
+        self.assertEqual(result, {'zh': 3, 'en': 3})  # Update expected result as needed
 
-  result_all = detect(text)
-  print('all:', result_all)
+    def test_skip_special_word(self):
+        text = "Hello, world! 我們的 DNA 是雙螺旋結構。" # DNA is an special word, that will passthought.
+        result = detect(text)
+        self.assertEqual(result, {'zh': 9, 'en': 2})  # Update expected result as needed
+
+if __name__ == '__main__':
+    unittest.main()
